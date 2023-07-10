5e7fca9a9178f5c38cd7109b984485bb90256d38
Daniel Loos <dloos@bgc-jena.mpg.de>
2023-07-10 17:14:57 +0200

    Add docstrings to list_processes and list_jobs

--- src/API.jl
+++ src/API.jl
@@ -1,5 +1,8 @@
 using DataFrames
 
+"""
+Lists all predefined processes and returns detailed process descriptions, including parameters and return values.
+"""
 function list_processes(connection::AbstractConnection)
     response = fetch(connection, "processes")
     ids = [x["id"] for x in response["processes"]]
@@ -7,6 +10,9 @@ function list_processes(connection::AbstractConnection)
     return processes
 end
 
+"""
+Lists all batch jobs submitted by a user.
+"""
 function list_jobs(connection::AuthorizedConnection)
     response = fetch(connection, "jobs")
     jobs = DataFrame(response["jobs"])
