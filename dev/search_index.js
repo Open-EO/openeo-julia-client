5f9566e324dfcad51d02d8b1ec52d32af0060db3
Documenter.jl <documenter@juliadocs.github.io>
2023-07-10 11:09:51 +0000

    build based on 2e86852

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OpenEOClient","category":"page"},{"location":"#OpenEOClient","page":"Home","title":"OpenEOClient","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OpenEOClient.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OpenEOClient]","category":"page"}]
+}
