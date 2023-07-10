68db47cb5c448f419f3c2b3ba282839f2295dced
Daniel Loos <dloos@bgc-jena.mpg.de>
2023-07-10 15:59:21 +0200

    Fix typo in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Test
 host = ENV["OPENEO_HOST"]
 version = ENV["OPENEO_VERSION"]
 username = ENV["OPENEO_USERNAME"]
-passwod = ENV["OPENEO_PASSWORD"]
+password = ENV["OPENEO_PASSWORD"]
 
 @testset "OpenEOClient.jl" begin
     connection = Connection(host, username, password, version)
