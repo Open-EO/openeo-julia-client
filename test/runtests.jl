e74dd2d24c787eaab7f93bd65e1f18480d7e7b93
Daniel Loos <dloos@bgc-jena.mpg.de>
2024-01-11 16:40:15 +0100

    Add julia syntax tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ username = ENV["OPENEO_USERNAME"]
 password = ENV["OPENEO_PASSWORD"]
 
 @testset "OpenEOClient.jl" begin
-    unauth_con = OpenEOClient.UnAuthorizedConnection(host, version)
+    unauth_con = OpenEOClient.UnAuthorizedCredentials(host, version)
     response = OpenEOClient.fetchApi(unauth_con, "/")
     @test "api_version" in keys(response)
     @test "backend_version" in keys(response)
@@ -19,6 +19,7 @@ password = ENV["OPENEO_PASSWORD"]
     c2 = connect(host, version, username, password)
     @test allequal([c1, c2] .|> x -> size(x.collections))
 
+    # test sequential workflow
     step1 = c2.load_collection(
         "COPERNICUS/S2", BoundingBox(west=16.06, south=48.06, east=16.65, north=48.35),
         ["2020-01-20", "2020-01-30"], ["B10"]
@@ -32,4 +33,26 @@ password = ENV["OPENEO_PASSWORD"]
     step3 = c2.save_result(step2, "JPEG", Dict())
     result = c2.compute_result(step3)
     @test result == "out.jpeg"
+
+    # test julia syntax workflow
+    cube = DataCube(c2, "COPERNICUS/S2",
+        BoundingBox(west=16.06, south=48.06, east=16.65, north=48.35),
+        ("2020-01-20", "2020-01-30"), ["B01", "B02"]
+    )
+
+    cube["B01"]
+    cube["B01"] + 1
+    1 + cube["B01"]
+    cube["B01"] + 1 + 2
+    cube + 1
+    1 + cube + 1
+    cube["B01"] + cube["B02"]
+    cube + cube
+    cube + 1
+    cube + 1 + 2
+    1 + cube
+    @test (cube["B01"]+1+1).call.arguments[:reducer] |> length == 3
+    @test (cube["B01"] + cube["B02"]).call.process_id == "merge_cubes"
+    @test (cube+2*3).call.arguments[:process] |> length == 1
+    @test cube2 = cube + 2 * 3 |> typeof == DataCube
 end
