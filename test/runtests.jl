19a6b030672fb5340bda34c80da6a872b00eb9ea
Daniel Loos <dloos@bgc-jena.mpg.de>
2024-09-11 14:53:59 +0200

    Adapt tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,15 +22,15 @@ password = ENV["OPENEO_PASSWORD"]
     # test sequential workflow
     step1 = c2.load_collection(
         "COPERNICUS/S2", BoundingBox(west=16.06, south=48.06, east=16.65, north=48.35),
-        ["2020-01-20", "2020-01-30"], ["B10"]
+        ["2020-01-20", "2020-01-30"]; bands=["B10"]
     )
     @test step1.id == "load_collection_tQ79zrFEGi8="
     @test step1.process_id == "load_collection"
     @test Set(keys(step1.arguments)) == Set([:bands, :id, :spatial_extent, :temporal_extent])
     @test step1.arguments[:bands] == ["B10"]
 
-    step2 = c2.reduce_dimension(step1, ProcessGraph("median"), "t", nothing)
-    step3 = c2.save_result(step2, "JPEG", Dict())
+    step2 = c2.reduce_dimension(step1, ProcessGraph("median"), "t")
+    step3 = c2.save_result(step2, "JPEG")
     result = c2.compute_result(step3)
     @test result == "out.jpeg"
 
